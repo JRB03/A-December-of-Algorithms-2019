9635775fa8e55f6c47073d03e6bb924c8bbf40f5
Jack <jbasinet21@hotchkiss.org>
2019-12-08 21:41:03 -0500

    7 clean

--- December-07/java_JRB03.java
+++ December-07/java_JRB03.java
@@ -4,44 +4,43 @@ import java.util.Queue;
 
 public class java_JRB03
 {
-  public static void main(String[] args)
-  {
-    Scanner reader = new Scanner(System.in);
-    Queue<String> queue = new LinkedList<>();
+   public static void main(String[] args)
+   {
+      Scanner reader = new Scanner(System.in);
+      Queue<String> queue = new LinkedList<>();
 
-    System.out.print("Enter N: ");
-    int n = Integer.parseInt(reader.nextLine());
+      System.out.print("Enter N: ");
+      int n = Integer.parseInt(reader.nextLine());
 
-    System.out.println("Enter '(token no, id)':");
-    for(int q = 0; q < n; q++)
-    {
-      String in = reader.nextLine();
-      queue.add(in);
-    }
-
-    System.out.print("Enter k: ");
-    String k = reader.nextLine();
-    for(String t : queue)
-    {
-      if(t.contains(k))
+      System.out.println("Enter '(token no, id)':");
+      for(int q = 0; q < n; q++)
       {
-        System.out.println(t);
+         String in = reader.nextLine();
+         queue.add(in);
       }
-    }
 
-    int no = 1;
-    while(queue.size() > 0)
-    {
-        if(queue.peek().contains(k))
-        {
-          queue.remove();
-          if(no != 1) { no--; }
-        }
-        else if(queue.peek().contains(Integer.toString(no)))
-        {
-          System.out.println(queue.poll());
-        }
-        no++;
-    }
-  }
+      System.out.print("Enter k: ");
+      String k = reader.nextLine();
+      for(String t : queue)
+      {
+         if(t.contains(k))
+         {
+            System.out.println(t);
+         }
+      }
+
+      int no = 1;
+      while(no <= queue.size())
+      {
+         for(String t : queue)
+         {
+            if(t.contains(Integer.toString(no)))
+            {
+               if(t.contains(k)) {}
+               else { System.out.println(t); }
+               no++;
+            }
+         }
+      }
+   }
 }
